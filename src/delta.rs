693824e743f4e9cbd117cba836a4e169acee0ad9
Lars Sjöberg <lsjoeberg@protonmail.com>
2024-10-20 14:12:18 +0200

    Remove `create` command
    Table creation is better done in a _data_ management system. The
    `deltactl` CLI should be limited to table management, which is
    requires an existing table.
    
    Table creation also requires a schema to be specified, which requires
    deciding a schema serialization format.

--- src/delta.rs
+++ src/delta.rs
@@ -1,53 +1,15 @@
-use deltalake::arrow::datatypes::Schema;
-use deltalake::kernel::StructField;
-use deltalake::{DeltaOps, DeltaTableError, TableProperty};
-use std::sync::Arc;
-
-fn arrow_schema_to_delta(schema: &Schema) -> Vec<StructField> {
-    let res: Result<Vec<_>, _> = schema
-        .fields
-        .iter()
-        .map(|f| StructField::try_from(f.as_ref()))
-        .collect();
-    res.unwrap() // error is infallible
-}
-
-pub async fn create_table(
-    uri: impl AsRef<str>,
-    schema: Arc<Schema>,
-) -> Result<(), DeltaTableError> {
-    let ops = DeltaOps::try_from_uri(uri).await?;
-
-    // Convert Arrow schema to Delta.
-    // TODO: Suitable in-mem representation of schema?
-    //  Would be deserialized from e.g. JSON or YAML.
-    //  NOTE: The Delta `StructField` type has serde to/from JSON.
-    let cols = arrow_schema_to_delta(&schema);
-
-    let table = ops
-        .create()
-        // TODO: Distinguish between URI, location, table name?
-        .with_configuration_property(TableProperty::MinReaderVersion, Some("3"))
-        .with_configuration_property(TableProperty::MinWriterVersion, Some("7"))
-        .with_columns(cols)
-        .await?;
-
-    println!("Created table: {table:#?}");
-
-    Ok(())
-}
+use deltalake::{DeltaOps, DeltaTableError};
 
 pub async fn optimize(uri: impl AsRef<str>) -> Result<(), DeltaTableError> {
     let table = deltalake::open_table(uri).await?;
     let ops = DeltaOps(table);
-    
+
     // TODO: Configure optimization properties: `.with_...`
     let (table, metrics) = ops.optimize().await?;
     println!("optimized table: {table:#?}\n{metrics:#?}");
 
     Ok(())
 }
-
 
 pub async fn vacuum(uri: impl AsRef<str>) -> Result<(), DeltaTableError> {
     let table = deltalake::open_table(uri).await?;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use clap::{Parser, Subcommand};
-use deltalake::arrow::datatypes::{DataType, Field, Schema};
-use std::sync::Arc;
 
 use deltactl::delta;
 
@@ -12,11 +10,6 @@ struct Args {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    /// Create a new Delta table.
-    Create {
-        /// URI pointing to the table storage.
-        uri: String,
-    },
     /// Optimize an existing table.
     Optimize {
         /// URI pointing to the table storage.
@@ -35,16 +28,9 @@ async fn main() {
     println!("{:?}", &args);
 
     match args.cmd {
-        Command::Create { uri } => {
-            let schema = Arc::new(Schema::new(vec![
-                Field::new("name", DataType::Utf8, false),
-                Field::new("value", DataType::Int32, false),
-            ]));
-            delta::create_table(uri, schema).await.unwrap();
-        }
         Command::Optimize { uri } => {
             delta::optimize(uri).await.unwrap();
-        },
+        }
         Command::Vacuum { uri } => {
             delta::vacuum(uri).await.unwrap();
         }
